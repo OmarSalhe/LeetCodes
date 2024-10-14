49b05f6e5a1ff7a0c9a7c3e64385129f4243da9e
Omar <osalhe12@gmail.com>
2024-10-14 08:22:13 -0400

    added 78 subsets w/o explanation

---
+++ Medium/078. Subsets/078. Subsets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def helper(i, sub=[]):
+            if i == len(nums):
+                res.append(sub[:])
+                return
+            # add
+            sub.append(nums[i])
+            helper(i+1, sub)
+            # remove
+            sub.pop()
+            helper(i+1, sub)
+        helper(0)
+        return res
+"""
+            [1,2,3]
+           /   |   \
+      [2,3]  [1,3]  [1,2]
+      /  \    / \     / \
+    [2]  [3] [1] [3] [1] [2]
+               |
+               |
+               v
+               []
+
+makes more sense, if you go from bottom-up, but becomes more intuitive if you recognize tree struct              
+"""
