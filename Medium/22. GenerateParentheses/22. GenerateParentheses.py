bc108b6f7afbd8b63a9200c6a7513977cdbe9de2
Omar <osalhe12@gmail.com>
2024-10-21 12:24:35 -0400

    added 22 w/o explanation

---
+++ Medium/22. GenerateParentheses/22. GenerateParentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        pairs = []
+        def bum_ahh_dfs(n, s='()', visited=set()):
+            visited.add(s)
+            if n == 0:
+                # add leaf to possible combo
+                pairs.append(s)
+                return
+            for i in range(len(s)):
+                if s[i] == ')' and (s[:i] + '()' + s[i:]) not in visited:
+                    bum_ahh_dfs(n-1, s[:i] + '()' + s[i:] )
+            bum_ahh_dfs(n-1, s + '()')
+            
+            # backtrack
+            return
+        bum_ahh_dfs(n-1) 
+        return pairs
